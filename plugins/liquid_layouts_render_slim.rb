f87567cd753f54f523d37d3c2d8c7c65214d3fd2
Justin Richter <jrichter@jetfive.com>
2012-08-15 18:02:05 -0500

    cleanup and clarification fir liquid_layouts_render_slim

--- plugins/liquid_layouts_render_slim.rb
+++ plugins/liquid_layouts_render_slim.rb
@@ -2,25 +2,6 @@ module Jekyll
 
 
   class Layout
-    include Convertible
-
-    # Gets the Site object.
-    attr_reader :site
-
-    # Gets/Sets the extension of this layout.
-    attr_accessor :ext
-
-    # Gets/Sets the Hash that holds the metadata for this layout.
-    attr_accessor :data
-
-    # Gets/Sets the content of this layout.
-    attr_accessor :content
-
-    # Initialize a new Layout.
-    #
-    # site - The Site.
-    # base - The String path to the source.
-    # name - The String filename of the post file.
     def initialize(site, base, name)
       @site = site
       @base = base
@@ -30,17 +11,10 @@ module Jekyll
 
       self.process(name)
       self.read_yaml(base, name)
+      #This allows slim templates in the layouts dir, or others like
+      #haml if the converter is in the plugins dir
       self.transform
       self.data
-    end
-
-    # Extract information from the layout filename.
-    #
-    # name - The String filename of the layout file.
-    #
-    # Returns nothing.
-    def process(name)
-      self.ext = File.extname(name)
     end
   end
 
